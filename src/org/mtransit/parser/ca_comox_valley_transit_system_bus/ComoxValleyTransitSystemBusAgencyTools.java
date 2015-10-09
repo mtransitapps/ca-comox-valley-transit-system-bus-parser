5a933fc673e48343057b8ea430e19f21e06380a1
Mathieu Méa <mathieu.mea@gmail.com>
2015-10-09 15:33:15 -0400

    Code cleaning

--- src/org/mtransit/parser/ca_comox_valley_transit_system_bus/ComoxValleyTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_comox_valley_transit_system_bus/ComoxValleyTransitSystemBusAgencyTools.java
@@ -164,7 +164,7 @@ public class ComoxValleyTransitSystemBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
-		if (mRoute.id == 9l) {
+		if (mRoute.getId() == 9l) {
 			if (gTrip.getDirectionId() == 0) {
 				mTrip.setHeadsignString(BEACH_BUS, gTrip.getDirectionId());
 				return;
@@ -206,13 +206,11 @@ public class ComoxValleyTransitSystemBusAgencyTools extends DefaultAgencyTools {
 
 	private static final Pattern STARTS_WITH_BOUND = Pattern.compile("(^(east|west|north|south)bound)", Pattern.CASE_INSENSITIVE);
 
-	private static final Pattern AT = Pattern.compile("( at )", Pattern.CASE_INSENSITIVE);
-	private static final String AT_REPLACEMENT = " / ";
 
 	@Override
 	public String cleanStopName(String gStopName) {
 		gStopName = STARTS_WITH_BOUND.matcher(gStopName).replaceAll(StringUtils.EMPTY);
-		gStopName = AT.matcher(gStopName).replaceAll(AT_REPLACEMENT);
+		gStopName = CleanUtils.CLEAN_AT.matcher(gStopName).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT);
 		gStopName = EXCHANGE.matcher(gStopName).replaceAll(EXCHANGE_REPLACEMENT);
 		gStopName = CleanUtils.cleanStreetTypes(gStopName);
 		gStopName = CleanUtils.cleanNumbers(gStopName);
